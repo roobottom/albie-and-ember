356049a1dcd2cc9243f84ddc799023b8e55885dd
Jon Roobottom <roo@roodesign.co.uk>
2016-08-23 20:39:26 +0100

    stubbed out a gulp static file build process

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,45 @@
 var gulp = require('gulp');
 var nodemon = require('gulp-nodemon');
+var connect = require('gulp-connect');
+var del = require('del');
 
-gulp.task('go', function () {
+var paths = {
+  templates: './pages/',
+  assets: './assets/',
+  target: './_site/'
+};
+
+gulp.task('connect', function() {
+  connect.server({
+    root: '_site',
+    livereload: true
+  });
+  console.log('dev server running at http://localhost:8080')
+});
+
+gulp.task('html', function () {
+  gulp.src('./_site/*.html')
+    .pipe(connect.reload());
+});
+
+gulp.task('copy',['clean'], function() {
+  gulp.src(['./pages/**/*.html','./assets/**/*.*'])
+  .pipe(gulp.dest('./_site/'));
+});
+
+gulp.task('watch', function () {
   nodemon({
-    script: 'app.js'
-  , ext: 'js html less'
-  , ignore: []
-  , env: {}
-  , watch: []
-  , tasks: []
+  script: './assets/js/index.js',
+  ext: 'js html less',
+  ignore: ['./_site/*'],
+  tasks: ['copy','html']
   })
 });
 
-gulp.task('default',['go']);
+gulp.task('clean', function () {
+  return del([
+    '_site/**/*'
+  ]);
+});
+
+gulp.task('default', ['connect', 'watch']);
