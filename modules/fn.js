31e517d6642aad1f585b4de82354a16db34797a5
Jon Roobottom <roo@roodesign.co.uk>
2016-09-12 09:04:20 +0100

    smooth colour scroll

--- modules/fn.js
+++ modules/fn.js
@@ -8,16 +8,21 @@
 
   var opts = {
     pages: [],
+    colors: [],
     pageSelector: '.pg',
     pageLoaded: 'pg--loaded',
     navSelector: '.toc_l'
   };
 
+  var pagesArray = d.querySelectorAll(opts.pageSelector);
+
   var init = function() {
     checkPageInView();
+    initColors();
     //handle scroll
     bindEvent(w,'scroll',function() {
       checkPageInView();
+      replaceBgColor();
     });
     //handle resize
     bindEvent(w,'resize',function() {
@@ -57,8 +62,7 @@
 
   var pagesFactory = function() {
     opts.pages = [];
-    var elements = d.querySelectorAll(opts.pageSelector);
-    Array.prototype.forEach.call(elements, function(el, i){
+    Array.prototype.forEach.call(pagesArray, function(el, i){
       opts.pages.push(new pageConstrutor(el,getElmentOffset(el)));
     });
   };
@@ -74,19 +78,76 @@
 
   var checkPageInView = function() {
     pagesFactory();
-    opts.pages.forEach(function(obj) {
+    var pageInView = null;
+    opts.pages.forEach(function(obj,i) {
       if(obj.offset <= documentScrollPosition()) {
         addClass(obj.el,opts.pageLoaded);
-      }
+        pageInView = {el: obj.el, id: i};
+      };
     });
+    return pageInView;
   };
+
+  //replace background colour on scroll http://stackoverflow.com/questions/16844723/change-background-color-on-scroll
+  var replaceBgColor = function() {
+    if(opts.colors.length > 0) {
+      var el = d.getElementsByTagName('body')[0]; // Element to be scrolled
+      var length = opts.colors.length; // Number of colors
+      var height = Math.round(documentHeight());
+
+      var i = Math.floor(documentScrollPosition() / height);  // Start color index
+      var dN = documentScrollPosition() % height / height;    // Which part of the segment between start color and end color is passed
+      var c1 = opts.colors[i].hsl;                            // Start color
+      var c2 = opts.colors[(i+1)%length].hsl;                 // End color
+      var h = c1[0] + Math.round((c2[0] - c1[0]) * dN);
+      var s = c1[1] + Math.round((c2[1] - c1[1]) * dN);
+      var l = c1[2] + Math.round((c2[2] - c1[2]) * dN);
+      d.body.style['background-color'] = ['hsl(', h, ', ', s+'%, ', l, '%)'].join('');
+    };
+  };
+  //init the colours.
+  var initColors = function() {
+    Array.prototype.forEach.call(pagesArray, function(el, i){
+      if(w.getComputedStyle) {
+        var rgb = w.getComputedStyle(el).getPropertyValue('background-color');
+        var rgbA = rgb.replace(' ','').replace(/rgba?\(/g,'').replace(/\)/g,'').split(',');
+        var hsl = rgbToHsl(parseInt(rgbA[0]),parseInt(rgbA[1]),parseInt(rgbA[2]));
+        //store page colours
+        opts.colors.push({rgb: rgb, hsl: hsl});
+        //remove css backgrounds
+        el.style.backgroundColor = 'transparent';
+      };
+    });
+    //set new bg
+    replaceBgColor();
+  };
+
+  //rbgToHsl
+  function rgbToHsl(r, g, b){
+    r /= 255, g /= 255, b /= 255;
+    var max = Math.max(r, g, b), min = Math.min(r, g, b);
+    var h, s, l = (max + min) / 2;
+    if(max == min){
+        h = s = 0; // achromatic
+    }else{
+        var d = max - min;
+        s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+        switch(max){
+            case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+            case g: h = (b - r) / d + 2; break;
+            case b: h = (r - g) / d + 4; break;
+        }
+        h /= 6;
+    }
+    return [Math.floor(h * 360), Math.floor(s * 100), Math.floor(l * 100)];
+}
 
   //smooth scroll.
   var handlePageNavigation = function() {
     var navLinks = d.querySelectorAll(opts.navSelector);
     Array.prototype.forEach.call(navLinks, function(el, i){
       bindEvent(el,'click',function(e) {
-        
+
       });
     });
   };
--- modules/i.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-(function(w, d, undefined){
-  d.addEventListener("DOMContentLoaded", function(e) {
-      var s = d.createElement('script');
-      s.setAttribute("type","text/javascript");
-      s.setAttribute("src", "/fn.js");
-      document.getElementsByTagName("head")[0].appendChild(s);
-  });
-})(window, document);
